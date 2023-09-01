92b76e5e13d0cb512b6a4093df6996048369f7b6
Szymon17 <jablonski.szymon21@gmail.com>
2023-09-01 20:51:45 +0200

    fixed validate fuction

--- server/src/utils/validate.ts
+++ server/src/utils/validate.ts
@@ -1,22 +1,25 @@
 export const validate = {
   email: (email: string): boolean => {
     const regExp = /^([\S]+)@([\w]+)\.([\w]+)$/;
-
-    return Boolean(email.match(regExp));
+    if (typeof email === "string") return Boolean(email.match(regExp));
+    else return false;
   },
 
   password: (password: string): boolean => {
     const regExp = /\d/;
-    return Boolean(password.match(regExp) && password.length > 4 && password.length < 15);
+    if (typeof password === "string") return Boolean(password.match(regExp) && password.length > 4 && password.length < 15);
+    else return false;
   },
 
   name: (name: string): boolean => {
     const regExp = /[\d\s!@#$%^&*()_+{}:;"'>?/.,<\\|]+/;
-    return Boolean(name.length > 2 && !name.match(regExp));
+    if (typeof name === "string") return Boolean(name.length > 2 && !name.match(regExp));
+    else return false;
   },
 
   phoneNumber: (phoneNumber: string): boolean => {
     const regExp = /^[\d]{9}$/;
-    return Boolean(phoneNumber.match(regExp));
+    if (typeof phoneNumber === "string") return Boolean(phoneNumber.match(regExp));
+    else return false;
   },
 };
