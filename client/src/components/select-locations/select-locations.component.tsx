d064bd9c28df11a6162dee3e855b3ac0dcb851f8
Szymon Jabłoński <jablonski.szymon21@gmail.com>
2023-10-16 14:32:08 +0200

    Fixed select-locaton component closing

--- client/src/components/select-locations/select-locations.component.tsx
+++ client/src/components/select-locations/select-locations.component.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useRef, useState } from "react";
 import { selectLocations } from "../../store/locations/locations.selector";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { getAppEvent } from "../../store/app/app.selectors";
 
 type selectLocationsTypes = { value: string; changeState: Function };
 
@@ -11,7 +12,18 @@ const SelectLocations: FC<selectLocationsTypes> = ({ value, changeState }) => {
   const locations = useAppSelector(selectLocations);
   const selectRef = useRef<HTMLDivElement>(null);
 
+  const appEvent = useAppSelector(getAppEvent);
+
   const [extend, setExtendState] = useState(false);
+
+  useEffect(() => {
+    const el = selectRef.current;
+
+    if (el) {
+      el.classList.remove("extend");
+      setExtendState(false);
+    }
+  }, [appEvent]);
 
   useEffect(() => {
     const el = selectRef.current;
@@ -35,7 +47,13 @@ const SelectLocations: FC<selectLocationsTypes> = ({ value, changeState }) => {
 
   return (
     <div ref={selectRef} className="selectLocations">
-      <div onClick={() => setExtendState(!extend)} className="selectLocations__selected">
+      <div
+        onClick={e => {
+          e.stopPropagation();
+          setExtendState(!extend);
+        }}
+        className="selectLocations__selected"
+      >
         {value}
       </div>
       <FontAwesomeIcon className="selectLocations__icon" icon={faCaretDown} />
